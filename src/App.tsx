f71def404e201bd8381fdbe010d74c46c0602026
Yash Hegde <yash4success@gmail.com>
2024-02-01 00:49:08 +0530

    login form initiated

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,26 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg"; */
 import "./App.css";
 import MultipleFileUploader from "./components/MultipleFileUploader";
+/* import Navbar from "./components/Navbar"; */
+/* import imagePath from "./assets/react.svg"; */
 /* import SingleFileUploader from "./components/SingleFileUploader"; */
 
 function App() {
   /* 
   const [count, setCount] = useState(0);
  */
+  /* const items = ["Home", "Product", "Service"]; */
   return (
     <>
       {/*  <SingleFileUploader /> */}
+      <div>
+        {/* <Navbar
+          brandName="My Brand"
+          imageSrcPath={imagePath}
+          navItems={items}
+        /> */}
+      </div>
       <MultipleFileUploader />
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </>
   );
 }
---
+++ src/components/Login.tsx
@@ -0,0 +1,3 @@
+/* https://medium.com/@prabhashi.mm/create-a-simple-react-app-typescript-with-login-register-pages-using-create-react-app-e5c12dd6db53
+
+use this article for login and register */
