a9dc0dec2376e823749673da21be5ec33208542d
Yash Hegde <yash4success@gmail.com>
2024-01-31 23:54:30 +0530

    tailwind added

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ function App() {
     <>
       {/*  <SingleFileUploader /> */}
       <MultipleFileUploader />
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </>
   );
 }
