cde46d22e69540be9ef58a249d7097618fce5408
Yash Hegde <yash4success@gmail.com>
2024-02-01 23:22:29 +0530

    db added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,13 @@
 import express from 'express';
 import authRouter from "./routes/authRouter";
+import connectUserDB from "./connections/userDB";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+connectUserDB();
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 8000;
@@ -9,3 +17,11 @@ app.listen(port, () => {
 });
 
 app.use(authRouter);
+app.use(bodyParser.json());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+app.use(cookieParser());
